b6d8435d5c23b0db23ced1ec03ee7378c27f9e2c
Bhargav Solanki <bks5881@rit.edu>
2018-09-27 21:29:59 -0400

    Updating readme

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # ticket
+
+This is a java application built using maven and mysql.
+
+The external libraries used are 
+
+1) GSON
+2) mysql connectors
+3) jooq for making table classes
+4) quartz-scheduler for automated releases of holds after
+
+## Steps to run
+
+1) A mysql database server is needed to run the application. currently the address is jdbc:mysql://localhost:3306/sales?serverTimezone=UTC
+
+2) In that server, please run the script "tables_queries.sql"
+
+3) Please change the database url in pom.xml, App.java and Release.java
+from "jdbc:mysql://localhost:3306/sales?serverTimezone=UTC" to your database url.
+Also assign the correct username and password for the same.
+
+4) Run a Project clean
+5) Maven clean, Maven build -> initialize
+6) Select the project and "Run as Java Application"
+
+The first statement you will see is "Items available".
+Where you will be shown the section, row and seat of the available seats along with their itemids.
+
+You should enter the number of items you want to reserve and press enter.
+
+then, provide the item_id of items you wish to reserve
+
+Then provide the email address with which you want to reserve the item
+with.
+NOTE: You wont be able to place items on hold for email adresses that do not exist in the customer table.
+
+If Hold is successful, you will be provided a unique orderid.
+
+Then, you will be asked to invoice the items you put on hold. Please note that the items can only be purchased when they are on hold, the invoice request must contain the exact orderid and same customer email address it was put on hold with.
+
+Then enter the Order id
+Next, text will ask you to enter the email address associated with your hold.
+
+If the invoice is successful, you will recieve an invoice id.
+
+If you fail to invoice out the held orders in 60 seconds, the hold will be released automatically.
+
+
+### Tables
+
+#### Event
+This table has eventid and associated event information. Each event at the venue is given a unique id
+
+#### Customer
+Each customer is given a unique cust_id. We later use this to map the email address
+
+### Item_reserve
+
+This table contains the orderid, item_ids and hold_expiration fields. The table will be populated and emptied based on hold expiration time and items which are placed on hold.
+
+### Item
+
+This is the most important table. items table consists of 
+
+1) Item_id : a unique item_id for each ticket.
+2) Barcods: The value of barcodes of the tickets.
+3) Shared flag: The stakeholders of the tickets, some times choose not to allow purchasing or invoicing of the tickets for buisenss/other reasons. So if the item is not shared, it wont be available to hold or invoice.
+4) ticketgroup: each ticket can be segregated in ticketgroups based on buiseness needs, and bulk actions can be taken. For eg: Tickets in a particaly section belong to one ticketgroup and are priced lower. than other. This functionality is not used in this project, but code can be modified to allow it.
+5) item_status: This field shows which statius the item is in. The acceptable values are "available", "ON_HOLD", "sold". This rule is not enforced currently. But if the item goes to any other status, it will be unavailable.
+
+6) external notes and internal notes: These fields are used during processing of holds and invoices. They can also be used to comments from stakeholders.
+
+7) Orderid: This field is associated to the unique hold id provided when hold is succesful.However, the value is assigned only when the item is invoiced out.
+
+8) Invoice ID: This filed is populated when we have a succesful invoice.
+
+Other fields are self explanatory in this table.
+
+
+
--- src/main/java/com/bhargav/sales/quickstart/App.java
+++ src/main/java/com/bhargav/sales/quickstart/App.java
@@ -1,7 +1,7 @@
 package com.bhargav.sales.quickstart;
 
 /**
- * Hello world!
+ * Tickets!
  *
  */
 import com.google.gson.Gson;
@@ -29,28 +29,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import com.bhargav.sales.quickstart.Release;
+
 public class App {
+
+	private static Scanner sc;
 
 	public static void main(String[] args) {
 		Gson gson = new Gson();
-		JobDetail release_job = JobBuilder.newJob(Release.class)
-				.withIdentity("Release", "Release").build();
-		Trigger trigger1 = TriggerBuilder.newTrigger()
-				.withIdentity("cronTrigger1", "group1")
-				.withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
-				.build();
-		
+		JobDetail release_job = JobBuilder.newJob(Release.class).withIdentity("Release", "Release").build();
+		Trigger trigger1 = TriggerBuilder.newTrigger().withIdentity("cronTrigger1", "group1")
+				.withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?")).build();
+
 		Scheduler scheduler1;
 		try {
 			scheduler1 = new StdSchedulerFactory().getScheduler();
 			scheduler1.start();
 			scheduler1.scheduleJob(release_job, trigger1);
 		} catch (SchedulerException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
-		System.out.println("Hello Worldlalal!");
 
 		try {
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sales?serverTimezone=UTC",
@@ -65,89 +62,91 @@ public class App {
 				String row = r.get(Item.ITEM.IROW);
 				String seat = r.get(Item.ITEM.SEAT);
 				Double amount = r.get(Item.ITEM.AMOUNT);
-                seat_data.put("item_id", "" + itemId);
-                seat_data.put("section", "" + section);
-                seat_data.put("row", "" + row);
-                seat_data.put("seat", "" + seat);
-                seat_data.put("amount", "" + amount);
-                dataMap.add(seat_data);
-				
+				seat_data.put("item_id", "" + itemId);
+				seat_data.put("section", "" + section);
+				seat_data.put("row", "" + row);
+				seat_data.put("seat", "" + seat);
+				seat_data.put("amount", "" + amount);
+				dataMap.add(seat_data);
 
 			}
-			if(dataMap.isEmpty()){
+			if (dataMap.isEmpty()) {
 				System.out.println("Event is sold out!");
-			}
-			else
-			{
+			} else {
 				System.out.println("Items available");
-			System.out.println(gson.toJson(dataMap));}
-			Scanner sc = new Scanner(System.in);
+				System.out.println(gson.toJson(dataMap));
+			}
+			sc = new Scanner(System.in);
 			System.out.println("Enter Number of items to reserve");
 			int quantity = 0;
 			quantity = sc.nextInt();
 			System.out.println("Enter itemids you want to reserve");
 			int items[] = new int[quantity];
-			for(int i=0;i<quantity;i++) {
+			for (int i = 0; i < quantity; i++) {
 				items[i] = sc.nextInt();
-				
+
 			}
 			String orderid = UUID.randomUUID().toString();
-			
+
 			System.out.println("Enter the email address of the customer");
 			String email = sc.next();
 			ArrayList<ItemRecord> updates = new ArrayList<ItemRecord>();
-			for(int i =0; i<quantity;i++) {
+			for (int i = 0; i < quantity; i++) {
 				ItemRecord record = new ItemRecord();
 				record.setItemId(items[i]);
 				record.setHoldComment(orderid);
-				int custid =  DSL.using(connection).selectFrom(Customer.CUSTOMER).where(Customer.CUSTOMER.EMAIL.eq(email)).fetch().get(0).getCustId();
+				int custid = DSL.using(connection).selectFrom(Customer.CUSTOMER)
+						.where(Customer.CUSTOMER.EMAIL.eq(email)).fetch().get(0).getCustId();
 				record.setInternalnotes("" + custid);
 				record.setItemStatus("ON_HOLD");
-				Integer hold_length = (int) ((System.currentTimeMillis() + 60000)/1000);
-			    String sql = String.format("insert into items_reserve(orderid, item_id, hold_expire, hold_cust_id) values(\'%s\', %d, %d, %d)",orderid, items[i],hold_length,custid);
-			    DSL.using(connection).execute(sql);
-			    updates.add(record);
-				
-				}
-			
+				Integer hold_length = (int) ((System.currentTimeMillis() + 60000) / 1000);
+				String sql = String.format(
+						"insert into items_reserve(orderid, item_id, hold_expire, hold_cust_id) values(\'%s\', %d, %d, %d)",
+						orderid, items[i], hold_length, custid);
+				DSL.using(connection).execute(sql);
+				updates.add(record);
+
+			}
+
 			DSLContext dsl_context = DSL.using(connection);
-			 int[] updates_data = dsl_context.batchUpdate(updates).execute();
-			 for(int i =0; i< updates_data.length;i++) {
-				 System.out.println(updates_data[i]);
-			 }
+			int[] updates_data = dsl_context.batchUpdate(updates).execute();
+			for (int i = 0; i < updates_data.length; i++) {
+				System.out.println(updates_data[i]);
+			}
 			System.out.println("Your hold was successful: \nOrderid:" + orderid);
-			System.out.println("Purchase your your holds in 20 less than seconds.Enter Orderid");
+			System.out.println("Purchase your your holds in  less than 60 .Enter Orderid");
 			String inputOrderId = sc.next();
 			System.out.println("Enter the email address for held order");
 			String inputEmail = sc.next();
 			String invoiceuuid = UUID.randomUUID().toString();
 			int invoiceId = invoiceuuid.hashCode();
-			int invoice_custid =  DSL.using(connection).selectFrom(Customer.CUSTOMER).where(Customer.CUSTOMER.EMAIL.eq(inputEmail)).fetch().get(0).getCustId();
-			String get_items_sql = String.format("select item_id from items_reserve where orderid = \'%s\' and hold_cust_id = %d",inputOrderId,invoice_custid );
+			int invoice_custid = DSL.using(connection).selectFrom(Customer.CUSTOMER)
+					.where(Customer.CUSTOMER.EMAIL.eq(inputEmail)).fetch().get(0).getCustId();
+			String get_items_sql = String.format(
+					"select item_id from items_reserve where orderid = \'%s\' and hold_cust_id = %d", inputOrderId,
+					invoice_custid);
 			Result<Record> itemsToInvoice = dsl_context.fetch(get_items_sql);
 			ArrayList<ItemRecord> invoiceList = new ArrayList<>();
-		    for(Record r: itemsToInvoice) {
-		    	Integer itemId = (Integer) r.getValue(0);
-		    	ItemRecord record = new ItemRecord();
-		    	record.setItemId(itemId);
-		    	record.setItemStatus("sold");
-		    	record.setPurchaseCustId(invoice_custid);
-		    	record.setInvoiceId(invoiceId);
-		    	record.setHoldComment(null);
+			for (Record r : itemsToInvoice) {
+				Integer itemId = (Integer) r.getValue(0);
+				ItemRecord record = new ItemRecord();
+				record.setItemId(itemId);
+				record.setItemStatus("sold");
+				record.setPurchaseCustId(invoice_custid);
+				record.setInvoiceId(invoiceId);
+				record.setOrderid(inputOrderId);
+				record.setHoldComment(null);
 
-		    	invoiceList.add(record);
-		    }
-		    dsl_context.batchUpdate(invoiceList).execute();
-		    if(invoiceList.isEmpty()) {
-		    	System.out.println("Invoice failure. No items reserved for that orderid and custid combination");
-		    }
-		    else
-		    {
-		    	System.out.println("Invoice successful for orderid: " + inputOrderId + " Invoiceid :" + invoiceId);
-		    	
-		    }
-			
-			
+				invoiceList.add(record);
+			}
+			dsl_context.batchUpdate(invoiceList).execute();
+			if (invoiceList.isEmpty()) {
+				System.out.println("Invoice failure. No items reserved for that orderid and custid combination");
+			} else {
+				System.out.println("Invoice successful for orderid: " + inputOrderId + " Invoiceid :" + invoiceId);
+
+			}
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
